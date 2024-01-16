888483607a5e34b9adb7f5927940097322dbf14d
Nikita <nikita.osipov59@yandex.ru>
2024-01-16 19:42:02 +0300

    Routes, NotFound and structure

---
+++ src/components/NotFoundBlock/NotFound.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./NotFoundBlock.module.scss";
+
+const NotFound = () => {
+  return <span className={styles.root}>404 NotFound</span>;
+};
+
+export default NotFound;
