4f68bafbd5a6ddd9d760fa2b7c02a863f008d132
Nikita <nikita.osipov59@yandex.ru>
2024-01-22 18:27:55 +0300

    Create NotFoundPage

--- src/components/NotFoundBlock/NotFound.jsx
+++ src/components/NotFoundBlock/NotFound.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import styles from "./NotFoundBlock.module.scss";
 
 const NotFound = () => {
-  return <span className={styles.root}>404 NotFound</span>;
+  return (
+    <>
+      <div className={styles.root}>
+        Ошибка 404
+        <span>Страница не найдена 😕</span>
+      </div>
+    </>
+  );
 };
 
 export default NotFound;
