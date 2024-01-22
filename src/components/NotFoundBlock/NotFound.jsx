ee5479095c47c397b395e8bdabfa19e7e55a8271
Nikita <nikita.osipov59@yandex.ru>
2024-01-22 20:43:39 +0300

    Redesign NotFoundPage

--- src/components/NotFoundBlock/NotFound.jsx
+++ src/components/NotFoundBlock/NotFound.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import styles from "./NotFoundBlock.module.scss";
 
 const NotFound = () => {
   return (
     <>
       <div className={styles.root}>
-        Ошибка 404
-        <span>Страница не найдена 😕</span>
+        <div className={styles.box}>
+          <img src="../../../public/notFound/background.png" alt="" />
+        </div>
+        <div className={styles.error}>
+          <h1>404</h1>
+          <span className={styles.test}>Страница не найдена 😕</span>
+          <Link className={styles.btn} to="/">
+            На главную
+          </Link>
+        </div>
       </div>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./components/assets/styles/_reset.scss";
-import "./components/assets/styles/_global.scss";
+import "./components/assets/styles/_global.module.scss";
 import Router from "./components/Router/Router.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router/>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<Router />);
