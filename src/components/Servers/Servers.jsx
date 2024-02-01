ba63541eb6b0ffb0c7ed45c9c54e3c8bc0dae063
Nikita <nikita.osipov59@yandex.ru>
2024-02-01 14:56:34 +0300

    some fix

--- src/components/Servers/Servers.jsx
+++ src/components/Servers/Servers.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 
 import Header from "../Header/Header";
-import styles from "./Servers.module.scss";
+import servers from "./Servers.module.scss";
+import global from "../assets/styles/_global.module.scss";
 
 const Servers = () => {
   return (
     <>
       <Header />
-      <div>Servers Page</div>
+      <>
+        <div className={global.container}>
+          <h1 className={servers.title}>Select a server</h1>
+        </div>
+      </>
     </>
   );
 };
