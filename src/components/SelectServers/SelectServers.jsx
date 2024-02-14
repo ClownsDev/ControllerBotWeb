74587abfe5b937deda6ca44d85a54c10e44aaa0b
phill <shchesniak.o@gmail.com>
2024-02-14 17:48:35 +0100

    i have tried

--- src/components/SelectServers/SelectServers.jsx
+++ src/components/SelectServers/SelectServers.jsx
@@ -10,8 +10,28 @@ const Servers = () => {
       <Header />
       <>
         <div className={global.container}>
+          <div className={servers.page}>
           <h1 className={servers.title}>Select a server</h1>
+          <div className={servers.grid}>
+          <div className={servers.case}>
+          <div className={servers.box}>
+            <div className={servers.imageAndLogo}>
+              <div className={servers.image}></div>
+              <div className={servers.logo}></div>
+            </div>
+            <div className={servers.description}>
+              <div className={servers.text}>
+                <h3 className={servers.serverName}>FuckMyMom</h3>
+                <div className={servers.serverRole}>Owner</div>
+              </div>
+              <button className={servers.buttonSetup}>Setup</button>
+            </div>
+          </div>
+          </div>
+          </div>
+          </div>
         </div>
+
       </>
     </>
   );
